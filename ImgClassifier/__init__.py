b31213445ee5d449d589f607cced3d01e85e3c11
amiralik16 <33791402+amiralik16@users.noreply.github.com>
2020-06-21 16:53:10 -0400

    ImgClassifier bot function initial commit

---
+++ ImgClassifier/__init__.py
@@ -0,0 +1,47 @@
+import logging
+import azure.functions as func
+import json
+import os
+import telegram
+from telegram import Update
+import requests
+
+# Import helper script
+from .predict import predict_image_from_url
+
+try:
+    token = os.getenv("BotToken")
+    bot = telegram.Bot(token)
+except Exception as e:
+    logging.exception("An exception was thrown!")
+    logging.info(f'ERROR: {e}')
+finally:
+    pass
+
+def extract_photo(bot, update):
+    file_id = update.message.photo[-1]
+    newFile = bot.getFile(file_id)
+    bot.sendMessage(chat_id=update.message.chat_id, text="Processing photo")
+    return newFile
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        logging.info(f'Recieved json is: {req.get_json()}')
+        update = Update.de_json(req.get_json(), bot)
+        botFile = extract_photo(bot, update)
+        results = predict_image_from_url(botFile._get_encoded_url())
+        bot.sendMessage(chat_id=update.message.chat_id, text=f"It is a {results['predictedTagName']}")
+
+        headers = {
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        }
+
+    except Exception as e:
+        logging.exception("An exception was thrown!")
+        logging.info(f'ERROR: {e}')
+    
+    finally:
+        results = {"Back to the drawing board"}
+        headers = {}
+    return func.HttpResponse(json.dumps(results), headers = headers)
